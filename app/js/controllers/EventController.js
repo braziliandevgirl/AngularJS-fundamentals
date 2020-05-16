d011308e83f5422ac0359afdcc5e54e0e084126f
JOLIVEIR <janile.dantas@sbi-technology.com>
2020-05-16 17:57:05 -0300

    Angular controllers & markup - Demo handling events

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -17,23 +17,34 @@ eventsApp.controller('EventController',
                     creatorName: 'Bob Smith',
                     duration: '1 hr',
                     level: 'Advanced',
-                    abstract: 'In this sesison will learn the ins and outs of directives'
+                    abstract: 'In this sesison will learn the ins and outs of directives',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Scopes for run and profit',
                     creatorName: 'John Doe',
                     duration: '30 mins',
                     level: 'Introductory',
-                    abstract: 'This section will take a closer look at scopes'
+                    abstract: 'This section will take a closer look at scopes',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Well behaved controller',
                     creatorName: 'Jane Doe',
                     duration: '2 hours',
                     level: 'Intermediate',
-                    abstract: 'Controllers are the beggining of everything Angularn does. Learn how to craft controllers that will win the respect of your friends and neighbors'
+                    abstract: 'Controllers are the beggining of everything Angularn does. Learn how to craft controllers that will win the respect of your friends and neighbors',
+                    upVoteCount: 0
                 }
             ]
+        }
+
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount++;
+        }
+
+        $scope.downVoteSession = function(session) {
+            session.upVoteCount--;        
         }
     }
 );
